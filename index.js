48f024d727eb307e9165d6bfefb72be9a59f7912
rubs019 <ruben.desert@eemi.com>
2018-05-18 17:49:04 +0200

    refactoring & add connectedPass

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
-const puppeteer = require('puppeteer');
 const dotenv = require('dotenv')
+const unConnectedPass = require('./lib/unConnectedPass')
 
 dotenv.load()
 
-
-function getUnconnectedTokenPass() {
-    return (async () => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-
-        await page.authenticate({username: process.env.USERNAME, password: process.env.PASSWORD})
-         await page.goto('https://dev.mycanal.fr/');
-        const cookies = await page.cookies()
-        const unconnectedTokenPass = cookies.find((elem) => {
-            if (elem.name === 's_pass_token') {
-                return elem.value
-            }
-        })
-
-        await browser.close();
-
-        return unconnectedTokenPass
-    })();
-}
-
-getUnconnectedTokenPass()
-.then((res) => console.log(res.value))
-.catch((err) => {
-    throw err
-})
+unConnectedPass

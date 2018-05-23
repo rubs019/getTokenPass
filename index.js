58d22e9bc2e4ce2283ad6d18ab1017484bdf1b28
rubs019 <ruben.desert@eemi.com>
2018-05-23 13:12:52 +0200

    merge update-code branch

--- index.js
+++ index.js
@@ -4,28 +4,59 @@ const puppeteer = require('puppeteer')
 
 const { DEV_MYCANAL_URL } = require('./constants')
 
-async function getUnconnectedTokenPass() {
-    const browser = await puppeteer.launch()
-    const page    = await browser.newPage()
 
-    await page.authenticate({username: process.env.USERNAME, password: process.env.PASSWORD})
+main()
+
+/**
+ * Entry point of app
+ * @return {Promise<void>}
+ */
+async function main() {
+    try {
+        const unconnectedTokenPassObj = await getUnconnectedTokenPass()
+
+        console.log(unconnectedTokenPassObj.value)
+    } catch (e) {
+        throw e
+    }
+}
+
+/**
+ * Use for fetch the tokenPass when we are not connected
+ * @return {Promise<number | * | T>}
+ */
+async function getUnconnectedTokenPass() {
+    const { browser, page } = await connectMyCanal()
+
     await page.goto(DEV_MYCANAL_URL)
 
     const cookies = await page.cookies()
 
-    const unconnectedTokenPass = cookies.find((elem) => {
-        if (elem.name === 's_pass_token') {
-            return elem.value
-        }
-    })
+    const unconnectedTokenPassObj = cookies.find((elem) => elem.name === 's_pass_token')
 
-    await browser.close()
+    await disconnectMyCanal(browser)
 
-    return unconnectedTokenPass
+    return unconnectedTokenPassObj
 }
 
-getUnconnectedTokenPass()
-    .then((res) => console.log(res.value))
-    .catch((err) => {
-        throw err
-    })
+/**
+ * Connect to the mycanal website
+ * @return {Promise<{browser, page}>}
+ */
+async function connectMyCanal() {
+    const browser = await puppeteer.launch()
+    const page    = await browser.newPage()
+
+    await page.authenticate({username: process.env.USERNAME, password: process.env.PASSWORD})
+
+    return { browser, page }
+}
+
+/**
+ * Use for disconnect the current app to mycanal
+ * @param browser
+ * @return {Promise<void>}
+ */
+async function disconnectMyCanal(browser) {
+    await browser.close()
+}
